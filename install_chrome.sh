5f3c32fd14e1778b1fd3b52c69deecaa3b8f208e
Shiyi Yin <38135561+yinshiyi@users.noreply.github.com>
2026-03-14 02:39:40 +0000

    add ci

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,8 +1,8 @@
-sudo apt update
-sudo apt install -y chromium-browser
-sudo apt install -y chromium
-pip install playwright
-playwright install chromium
+# sudo apt update
+# sudo apt install -y chromium-browser
+# sudo apt install -y chromium
+# pip install playwright
 sudo rm /etc/apt/sources.list.d/yarn.list
 sudo apt update
 playwright install-deps
+playwright install chromium
